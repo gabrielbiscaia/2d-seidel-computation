a2e3f521a24cbebbbc095cdd9ef186666b78cde6
gabrielbiscaia <gabrielbiscaia23@hotmail.com>
2023-08-10 13:40:48 -0300

    alteração da versão do polybench 4.2 -> 3.2

--- polybench-c-3.2/stencils/jacobi-2d-imper/jacobi-2d-imper.c
+++ polybench-c-3.2/stencils/jacobi-2d-imper/jacobi-2d-imper.c
@@ -1,14 +1,10 @@
 /**
- * This version is stamped on May 10, 2016
+ * jacobi-2d-imper.c: This file is part of the PolyBench/C 3.2 test suite.
  *
- * Contact:
- *   Louis-Noel Pouchet <pouchet.ohio-state.edu>
- *   Tomofumi Yuki <tomofumi.yuki.fr>
  *
+ * Contact: Louis-Noel Pouchet <pouchet@cse.ohio-state.edu>
  * Web address: http://polybench.sourceforge.net
  */
-/* seidel-2d.c: this file is part of PolyBench/C */
-
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
@@ -18,19 +14,24 @@
 #include <polybench.h>
 
 /* Include benchmark-specific header. */
-#include "seidel-2d.h"
+/* Default data type is double, default size is 20x1000. */
+#include "jacobi-2d-imper.h"
 
 
 /* Array initialization. */
 static
 void init_array (int n,
-		 DATA_TYPE POLYBENCH_2D(A,N,N,n,n))
+		 DATA_TYPE POLYBENCH_2D(A,N,N,n,n),
+		 DATA_TYPE POLYBENCH_2D(B,N,N,n,n))
 {
   int i, j;
 
   for (i = 0; i < n; i++)
     for (j = 0; j < n; j++)
-      A[i][j] = ((DATA_TYPE) i*(j+2) + 2) / n;
+      {
+	A[i][j] = ((DATA_TYPE) i*(j+2) + 2) / n;
+	B[i][j] = ((DATA_TYPE) i*(j+3) + 3) / n;
+      }
 }
 
 
@@ -43,34 +44,35 @@ void print_array(int n,
 {
   int i, j;
 
-  POLYBENCH_DUMP_START;
-  POLYBENCH_DUMP_BEGIN("A");
   for (i = 0; i < n; i++)
     for (j = 0; j < n; j++) {
-      if ((i * n + j) % 20 == 0) fprintf(POLYBENCH_DUMP_TARGET, "\n");
-      fprintf(POLYBENCH_DUMP_TARGET, DATA_PRINTF_MODIFIER, A[i][j]);
+      fprintf(stderr, DATA_PRINTF_MODIFIER, A[i][j]);
+      if ((i * n + j) % 20 == 0) fprintf(stderr, "\n");
     }
-  POLYBENCH_DUMP_END("A");
-  POLYBENCH_DUMP_FINISH;
+  fprintf(stderr, "\n");
 }
 
 
 /* Main computational kernel. The whole function will be timed,
    including the call and return. */
 static
-void kernel_seidel_2d(int tsteps,
-		      int n,
-		      DATA_TYPE POLYBENCH_2D(A,N,N,n,n))
+void kernel_jacobi_2d_imper(int tsteps,
+			    int n,
+			    DATA_TYPE POLYBENCH_2D(A,N,N,n,n),
+			    DATA_TYPE POLYBENCH_2D(B,N,N,n,n))
 {
   int t, i, j;
 
 #pragma scop
-  for (t = 0; t <= _PB_TSTEPS - 1; t++)
-    for (i = 1; i<= _PB_N - 2; i++)
-      for (j = 1; j <= _PB_N - 2; j++)
-	A[i][j] = (A[i-1][j-1] + A[i-1][j] + A[i-1][j+1]
-		   + A[i][j-1] + A[i][j] + A[i][j+1]
-		   + A[i+1][j-1] + A[i+1][j] + A[i+1][j+1])/SCALAR_VAL(9.0);
+  for (t = 0; t < _PB_TSTEPS; t++)
+    {
+      for (i = 1; i < _PB_N - 1; i++)
+	for (j = 1; j < _PB_N - 1; j++)
+	  B[i][j] = 0.2 * (A[i][j] + A[i][j-1] + A[i][1+j] + A[1+i][j] + A[i-1][j]);
+      for (i = 1; i < _PB_N-1; i++)
+	for (j = 1; j < _PB_N-1; j++)
+	  A[i][j] = B[i][j];
+    }
 #pragma endscop
 
 }
@@ -84,16 +86,17 @@ int main(int argc, char** argv)
 
   /* Variable declaration/allocation. */
   POLYBENCH_2D_ARRAY_DECL(A, DATA_TYPE, N, N, n, n);
+  POLYBENCH_2D_ARRAY_DECL(B, DATA_TYPE, N, N, n, n);
 
 
   /* Initialize array(s). */
-  init_array (n, POLYBENCH_ARRAY(A));
+  init_array (n, POLYBENCH_ARRAY(A), POLYBENCH_ARRAY(B));
 
   /* Start timer. */
   polybench_start_instruments;
 
   /* Run kernel. */
-  kernel_seidel_2d (tsteps, n, POLYBENCH_ARRAY(A));
+  kernel_jacobi_2d_imper (tsteps, n, POLYBENCH_ARRAY(A), POLYBENCH_ARRAY(B));
 
   /* Stop and print timer. */
   polybench_stop_instruments;
@@ -105,6 +108,7 @@ int main(int argc, char** argv)
 
   /* Be clean. */
   POLYBENCH_FREE_ARRAY(A);
+  POLYBENCH_FREE_ARRAY(B);
 
   return 0;
 }
